2bcc1d059c95f02132f9ae6a36574e104ef8db9c
manugrana2 <manugrana2@gmail.com>
2023-05-29 22:34:09 -0500

    Completed C# Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,54 @@ using System;
 
 class Program
 {
+    public static void StartGame()
+    {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int guesses = 0;
+        bool guessed = false;
+        while (guessed == false)
+        {
+            Console.WriteLine("What is your guess (1-100)?");
+            string guess = Console.ReadLine();
+            int guessNum;
+            if (int.TryParse(guess, out guessNum))
+            {
+                if (guessNum == number)
+                {
+                    Console.WriteLine("You guessed it!");
+                    guessed = true;
+                }
+                else if (guessNum < number)
+                {
+                    Console.WriteLine("Higher");
+                    guesses++;
+                }
+                else{
+                    Console.WriteLine("Lower");
+                    guesses++;
+                }
+            }
+
+        }
+        Console.WriteLine($"Congrats!, it took you {guesses} guesses");
+
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        StartGame();
+        Console.WriteLine("---------------------------------------");
+        bool finish = false;
+        while(finish == false){
+            Console.WriteLine("Do you want to play again? Write yes or no.");
+            string response = Console.ReadLine();
+            if(response == "yes"){
+                StartGame();
+            }else if (response == "no"){
+                Console.WriteLine("Thanks for playing. Finishing the program, bye!");
+                finish = true;
+            }
+        }
+
     }
 }
