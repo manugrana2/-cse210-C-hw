b86300083372445cd6154fcd471435b315c8c011
manugrana2 <manugrana2@gmail.com>
2023-06-30 01:00:06 -0500

    Completed Week 04 Learning Activity

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,19 @@
+public class Assignment{
+    private string _studentName;
+    private string _topic;
+
+    protected Assignment(string studentName, string topic){
+        _studentName = studentName;
+        _topic = topic;
+    }
+    public string GetSummary(){
+        return _studentName+" - "+_topic;
+
+    }
+    public string GetStudentName(){
+        return _studentName;
+    }
+    public string GetTopic(){
+        return _topic;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment mathtest = new MathAssignment("Roberto Perez", "Graphs", "7", "5-10");
+        Console.WriteLine(mathtest.GetSummary());
+        Console.WriteLine(mathtest.GetHomeworkList());
+        WritingAssignment writingtest = new WritingAssignment("Maria Florez", "Industrial Revolution", "The effects on industrial revolution on goods access");
+        Console.WriteLine(writingtest.GetSummary());
+        Console.WriteLine(writingtest.GetWritingInformation());
     }
 }
